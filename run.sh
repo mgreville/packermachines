b5b106fc55a379a0e4aabd2f0a63077875d2a269
Marcus Greville <marcus.greville@gmail.com>
2017-01-07 14:45:02 +1100

    Fixed incorrect filename

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 BUILD_NUMBER=${BUILD_NUMBER:-0}
 
 #This creates the server1.iso used for the cloud-init first boot configuration
-./mk.cloudinit.server.sh --hostname server-${BUILD_NUMBER} --ssh-key ~/.ssh/id_rsa.pub \
+./mk.cloudinit.image.sh --hostname server-${BUILD_NUMBER} --ssh-key ~/.ssh/id_rsa.pub \
 --user-data userdata.set.role.sh server-${BUILD_NUMBER}.iso
 
 if [ -d output_centos ]; then rm -fr output_centos; fi
